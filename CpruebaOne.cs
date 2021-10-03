7b1c846de91b277b457d84a46697a87140ac88f1
Yosandi <elyosandireyes@gmail.com>
2021-10-02 20:06:17 -0400

    Funcion statica para sumar añadida

--- CpruebaOne.cs
+++ CpruebaOne.cs
@@ -6,5 +6,15 @@ namespace LearnGit
 {
     class CpruebaOne
     {
+
+        public static int sumar (int valor1,int valor2)
+            {
+            return valor1+valor2;
+            }
+
+
+
+
+
     }
 }
